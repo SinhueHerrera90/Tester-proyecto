6793faaf37cf8d4d5d030ecb769061d2fc06c873
SinhueHerrera90 <116138705+SinhueHerrera90@users.noreply.github.com>
2023-05-15 23:07:05 +0000

    removed application root

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-root to: "application#root"
-  get("/homepage", {:controller => "posts", :action => "home"})
+
+  get("/", {:controller => "posts", :action => "home"})
   # Routes for the Location resource:
 
   # CREATE
@@ -13,17 +13,17 @@ root to: "application#root"
   
   # UPDATE
   
-  post("/modify_location/:path_id", { :controller => "locations", :action => "update" })
+  #post("/modify_location/:path_id", { :controller => "locations", :action => "update" })
   
   # DELETE
-  get("/delete_location/:path_id", { :controller => "locations", :action => "destroy" })
+  #get("/delete_location/:path_id", { :controller => "locations", :action => "destroy" })
 
   #------------------------------
 
   # Routes for the Post resource:
 
   # CREATE
-  post("/insert_post", { :controller => "posts", :action => "create" })
+  #post("/insert_post", { :controller => "posts", :action => "create" })
           
   # READ
 
@@ -34,29 +34,29 @@ root to: "application#root"
   
   # UPDATE
   
-  post("/modify_post/:path_id", { :controller => "posts", :action => "update" })
+  #post("/modify_post/:path_id", { :controller => "posts", :action => "update" })
   
   # DELETE
-  get("/delete_post/:path_id", { :controller => "posts", :action => "destroy" })
+  #get("/delete_post/:path_id", { :controller => "posts", :action => "destroy" })
 
   #------------------------------
 
   # Routes for the Activity resource:
 
   # CREATE
-  post("/insert_activity", { :controller => "activities", :action => "create" })
+  #post("/insert_activity", { :controller => "activities", :action => "create" })
           
   # READ
-  get("/activities", { :controller => "activities", :action => "index" })
+  #get("/activities", { :controller => "activities", :action => "index" })
   
   get("/activities/:path_id", { :controller => "activities", :action => "show" })
   
   # UPDATE
   
-  post("/modify_activity/:path_id", { :controller => "activities", :action => "update" })
+  #post("/modify_activity/:path_id", { :controller => "activities", :action => "update" })
   
   # DELETE
-  get("/delete_activity/:path_id", { :controller => "activities", :action => "destroy" })
+  #get("/delete_activity/:path_id", { :controller => "activities", :action => "destroy" })
 
   #------------------------------
 
